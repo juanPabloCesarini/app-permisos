3221efd79bab561cfdecb46a33b2437f01f889bd
juanPabloCesarini <juanpcesarini@hotmail.com>
2020-11-19 14:57:55 -0300

    Estructura del proyecto

---
+++ app/init.php
@@ -0,0 +1,17 @@
+<?php
+  session_start();
+  // se cargan las librerías
+  require_once 'config/configurar.php';
+  require_once 'helpers/url_helper.php';
+
+  //require_once "lib/Base.php";
+  //require_once "lib/Controller.php";
+  //require_once "lib/Core.php";
+
+  // autoload php
+
+  spl_autoload_register(function($nombreClase){
+    require_once 'lib/'.$nombreClase.'.php';
+  });
+  
+?>
