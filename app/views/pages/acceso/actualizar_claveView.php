1d27e14e5cb52ffbcf5d6e2449bbf9b6a009cf43
juanPabloCesarini <juanpcesarini@hotmail.com>
2020-11-20 21:04:31 -0300

    Cambios en vistas

--- app/views/pages/acceso/actualizar_claveView.php
+++ app/views/pages/acceso/actualizar_claveView.php
@@ -3,38 +3,49 @@
 <div class="container-fluid px-1 py-5 mx-auto">
       <div class="row d-flex justify-content-center">
           <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
-              <div class="card form-contraseña">
-                  <form action="<?php echo RUTA_URL;?>/FSF/setear_clave_nueva/" method="POST">
-                  <div>
+              <div class="card form-contraseña">    
                     <div class="card-header">
-                        <h3>Cambie su Contraseña</h3>
+                        <h2 class="subtitulo">Cambie su Contraseña</h2>
                     </div>
                     <div class="card-body">
-                        <div class="form-group">
-                          <label for="nick">Nickname</label>
-                          <input type="text" id="nick" name="nick" placeholder="Ingrese su nickname" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd">Contraseña actual</label>
-                            <input type="password" id="pwd" name="pwd_act" placeholder="Ingrese su contraseña actual" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd">Nueva contraseña</label>
-                            <input type="password" id="pwd" name="pwd1" placeholder="Ingrese su contraseña nueva" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd">Valide su nueva contraseña</label>
-                            <input type="password" id="pwd" name="pwd2" placeholder="Reescriba su contraseña" class="form-control">
-                        </div>
-                        <?php if (!empty($datos['msj'])==1):?>
-                          <p class="parrafo"><b><?php echo $datos['msj'];?></b></p>
-                        <?php endif;?>
-                        <div class="form-group">
-                            <input type="submit" class="btn-guardar" data-toggle="modal" data-target="#myModal" Value="Guardar Cambios">
-                        </div>
+                        <form action="<?php echo RUTA_URL;?>/FSF/setear_clave_nueva/" method="POST">
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-user parrafo"></i></span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Usuario" name="nick">
+                            </div>
+                          
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-key parrafo"></i></span>
+                                </div>
+                                <input type="password" class="form-control" placeholder="Contraseña actual" name="pwd_act" >
+                            </div>
+
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-key parrafo"></i></span>
+                                </div>
+                                <input type="password" class="form-control" placeholder="Contraseña nueva" name="pwd1" >
+                            </div>
+
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-key parrafo"></i></span>
+                                </div>
+                                <input type="password" class="form-control" placeholder=" Reingrese contraseña " name="pwd2" >
+                            </div>
+                            
+                  
+                            <?php if (!empty($datos['msj'])==1):?>
+                                <p class="text-danger text-center"><b><?php echo $datos['msj'];?></b></p>
+                            <?php endif;?>
+                            <div class="form-group">
+                                <input type="submit" class="btn-guardar" Value="Enviar Cambios">
+                            </div>
+                        </form>
                     </div>
-                </div>
-                </form>
               </div>
           </div>
       </div>
--- app/views/pages/acceso/loginView.php
+++ app/views/pages/acceso/loginView.php
@@ -4,9 +4,7 @@
       <div class="d-flex justify-content-center h-100">
         <div class="card form-login">
           <div class="card-header">
-            
-            <h2 class="subtitulo">Iniciar Sesión</h4>
-
+              <h2 class="subtitulo">Iniciar Sesión</h2>
           </div>
           <div class="card-body">
             <form action="<?php echo RUTA_URL;?>/FSF/permitir_entrar/" method="POST">
@@ -15,34 +13,33 @@
                   <span class="input-group-text"><i class="icon-user parrafo"></i></span>
                 </div>
                 <input type="text" class="form-control" placeholder="Usuario" name="nick">
-
               </div>
               <div class="input-group form-group">
                 <div class="input-group-prepend">
                   <span class="input-group-text"><i class="icon-key parrafo"></i></span>
                 </div>
                 <input type="password" class="form-control" placeholder="Contraseña" name="pass" >
-            </div>
-            
-              <div class="form-group justify-content-center d-flex">
-                <input type="submit" value="Entrar" class="float-right btn-guardar">
               </div>
+            
               <?php if (!empty($datos['msj'])==1):?>
               <div class="d-flex justify-content-center">
-              <p class="text-danger text-center"><b><?php echo $datos['msj'];?></b></p>
-            </div>
+                  <p class="text-danger text-center"><b><?php echo $datos['msj'];?></b></p>
+              </div>
             <?php endif;?>
-             </form>
+        
+              <div class="d-flex justify-content-center">
+                <a href="<?php echo RUTA_URL;?>/FSF/modificar_clave/" class="ml-2 mr-2 mt-1" style="text-decoration:none">Antes de loguearte por primera vez debes cambiar tu contraseña</a>
+              </div>  
+              <div class="d-flex justify-content-center">  
+                <a href="<?php echo RUTA_URL;?>/FSF/pedir_clave_nueva/" class="ml-2 mr-2 mt-1" style="text-decoration:none">Te olvidaste la contraseña?</a>
+              </div>
+              <div class="form-group justify-content-center d-flex">
+                  <input type="submit" value="Entrar" class="btn-guardar">
+              </div>
+            </form>
           </div>
-            <div class="d-flex justify-content-center">
-              <a href="<?php echo RUTA_URL;?>/FSF/modificar_clave/" class="ml-2 mr-2" style="text-decoration:none">Antes de loguearte por primera vez debes cambiar tu contraseña</a>
-            </div>
-            <div class="d-flex justify-content-center">
-              <a href="<?php echo RUTA_URL;?>/FSF/pedir_clave_nueva/" class="ml-2 mr-2" style="text-decoration:none">Te olvidaste la contraseña?</a>
-            </div>
-          
         </div>
       </div>
-    </div>
+</div>
 
 <?php require RUTA_APP .'/views/inc/footer.php';?>
