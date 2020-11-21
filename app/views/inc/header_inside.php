4940d77fb33eabc051da31ac543d10fbd2206868
juanPabloCesarini <juanpcesarini@hotmail.com>
2020-11-21 10:43:32 -0300

    Cambio diseño

--- app/views/inc/header_inside.php
+++ app/views/inc/header_inside.php
@@ -12,9 +12,12 @@
    <title><?php echo NOMBRESITIO;?> </title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark m-3">
+<div class="text-right">
+  <span class="icon-user text-success m-3"><?php echo ' ' .$_SESSION['nick'];?></span>
+</div>
+<nav class="navbar navbar-expand navbar-dark bg-dark m-3">
   <span class="navbar-brand mb-0 h1"><img src="<?php echo RUTA_URL;?>/img/logo-FSF.png" id="logo" />
-   <?php echo "<p class='parrafo text-white'>Hola " .$_SESSION['nick']."</p>";?>
+   
   </span>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -22,15 +25,20 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo RUTA_URL;?>/FSF/home/"><span class="icon-home3 parrafo text-white "> Home</spam></a>
+        <a class="nav-link" href="<?php echo RUTA_URL;?>/FSF/home/"><span class="icon-home3 text-white "> Home</spam></a>
       </li>
-      <li class="nav-item">
-        
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <span class="icon-user text-white"> Mi Perfi</span>      
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="<?php echo RUTA_URL;?>/FSF/editar_empleado/<?php echo $_SESSION['idEmpl'];?>">Actualizar Perfil</a>
+        </div>
       </li>
       
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <span class="icon-spoon-knife parrafo text-white"> Administrar Restaurantes</span>      
+         <span class="icon-spoon-knife text-white"> Administrar Restaurantes</span>      
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="<?php echo RUTA_URL;?>/FSF/alta_resto/">Dar de Alta</a>
@@ -41,7 +49,7 @@
 
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <span class="icon-newspaper parrafo text-white"> Administrar Blog</span>      
+         <span class="icon-newspaper  text-white"> Administrar Blog</span>      
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="<?php echo RUTA_URL;?>/FSF/alta_blog/">Dar de Alta</a>
@@ -49,11 +57,15 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo RUTA_URL;?>/FSF/logout/"><span class="icon-exit parrafo text-white"> Salir</spam></a>
+        <a class="nav-link" href="<?php echo RUTA_URL;?>/FSF/logout/"><span class="icon-exit text-white"> Salir</spam></a>
       </li>
     </ul>
   </div>
-</nav>
+  
+  <span class="navbar-brand mb-0 h1"><img src="<?php echo RUTA_URL;?>/img/logo-FSF.png" id="logo" />
+  </nav>
+  
+
    
    
       
--- app/views/pages/acceso/homeView.php
+++ app/views/pages/acceso/homeView.php
@@ -2,13 +2,18 @@
 
 <?php if ($_SESSION['rol'] =="1") : ?>
    <a class="nav-link" href="<?php echo RUTA_URL;?>/FSF/nuevo_empleado/"><span class="icon-user-plus parrafo text-white ml-3"> Nuevo Usuario</span></a>
-<p class="parrafo text-white ml-3">Nómina usuarios</p>
+<p class="parrafo text-white text-center">Nómina usuarios</p>
+<div class="container">
 <table class="table bg-light">
    <thead>
       <tr>
          <th class="subtitulo">Nickname</th>
          <th class="subtitulo">Nombre</th>
          <th class="subtitulo">Apellido</th>
+         <th class="text-center"><span class=" subtitulo icon-unlocked text-primary"></span> </th>
+         <th class="text-center"><span class=" subtitulo icon-pencil text-success"></span> </th>
+         <th class="text-center"><span class=" subtitulo icon-bin2 text-danger"></span> </th>
+
       </tr>
    </thead>
    <tbody>
@@ -20,19 +25,23 @@
             <td><?php echo $empleado->apellido;?></td>
             
             <?php if ($empleado->rol == "2") : ?>
-                   <td><a href="<?php echo RUTA_URL;?>/FSF/reasignar_permisos/<?php echo $empleado->IDempleado;?>"><span class="btn icon-unlocked text-warning"> Reasignar permisos</span></a></td>
+                   <td><a href="<?php echo RUTA_URL;?>/FSF/reasignar_permisos/<?php echo $empleado->IDempleado;?>" class="btn btn-outline-primary">Reasignar permisos</a></td>
             <?php endif;?>
-            <td><a href="<?php echo RUTA_URL;?>/FSF/editar/<?php echo $empleado->IDempleado;?>"><span class="btn icon-pencil text-success"> Editar</span></a></td>
+            <td><a href="<?php echo RUTA_URL;?>/FSF/editar_empleado/<?php echo $empleado->IDempleado;?>"class="btn btn-outline-success ">Editar</a></td>
             
-            <td><a href="<?php echo RUTA_URL;?>/FSF/eliminar_empleado/<?php echo $empleado->IDempleado;?>" onclick=confirmar(<?php echo $empleado->IDempleado .','.$empleado->nombre.','.$empleado->apellido;?>)"><span class="btn icon-bin2 text-danger"> Borrar</span></a></td>
+            <td><a href="#" onclick=confirmar(<?php echo $empleado->IDempleado;?>,'<?php echo $empleado->nombre;?>','<?php echo $empleado->apellido;?>') class="btn btn-outline-danger">Borrar</a></td>
             
          </tr>
       <?php endif;?>
    <?php endforeach; ?>
    </tbody>
 </table>
+</div>
    <?php else:?>
-      <h3><div class="row d-flex justify-content-center">Selecciona la opción para comenzar a trabajar</h3>
-      <h3><div class="row d-flex justify-content-center">Vista Empleado</h3>
+   <div class="container m-5">
+      <p class="subtitulo text-light text-center">
+         Friends Software Factory<br>te desea una jornada colmada de éxito
+      </p>
+   </div>
    <?php endif;?>
 <?php require RUTA_APP .'/views/inc/footer.php';?>
