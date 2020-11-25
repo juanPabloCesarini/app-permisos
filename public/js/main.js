e3dccc9e0154e3115bd7cce9dea804ea576123e1
juanPabloCesarini <juanpcesarini@hotmail.com>
2020-11-25 00:34:02 -0300

    Ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,20 @@
-function confirmar (id,nombre,apellido) {
-   if (apellido==null){
-      if (confirm('ATENCION!! Estas seguro de eliminar a: ' + nombre)){
-         window.location.href = '../eliminar_resto/' + id;
-      }
-   }else{
-      if (confirm('ATENCION!! Estás seguro de eliminar a: ' + nombre + ' ' + apellido)) {
-         window.location.href = '../eliminar_empleado/' + id;
-      }
+function confirmar (id,nombre,apellido,tabla) {
+   switch (tabla){
+      case 'post':
+         if (confirm('ATENCION!! Estas seguro de eliminar el post con título: ' + nombre)){
+            window.location.href = '../eliminar_post/' + id;
+         }
+         break;
+      case 'resto':
+         if (confirm('ATENCION!! Estas seguro de eliminar a: ' + nombre)){
+            window.location.href = '../eliminar_resto/' + id;
+         }
+         break;
+      case 'empleado':
+         if (confirm('ATENCION!! Estás seguro de eliminar a: ' + nombre + ' ' + apellido)){
+            window.location.href = '../eliminar_empleado/' + id;
+         }
+         break;
+   
    }
 }
-  /*  if (confirm('ATENCION!! Estás seguro de eliminar a: ' + nombre + ' ' + apellido)) {
-      window.location.href = '../eliminar_empleado/' + id;
-   } */
-
-
-/*  function confirmar2(idR,nombreR){
-   if (confirm('ATENCION!! Estas seguro de eliminar a: ' + nombreR)){
-      window.location.href = '../eliminar_resto/' + idR;
-   }
-}  */
