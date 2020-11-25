ad2b8ddfed80e54c9e041ca20671f9ee6c7cd488
juanPabloCesarini <juanpcesarini@hotmail.com>
2020-11-25 09:22:19 -0300

    Configuración PHPMAILER

--- app/init.php
+++ app/init.php
@@ -3,7 +3,13 @@
   // se cargan las librerías
   require_once 'config/configurar.php';
   require_once 'helpers/url_helper.php';
-  //require_once 'helpers/includeMails/class.phapmailer.php';
+  require_once 'helpers/generador_colores.php';
+  require_once 'helpers/generador_clave_helper.php';
+  require_once 'includesMails/class.phpmailer.php.';
+  require_once 'includesMails/class.pop3.php';
+  require_once 'includesMails/class.smtp.php';
+
+  
 
   //require_once "lib/Base.php";
   //require_once "lib/Controller.php";
